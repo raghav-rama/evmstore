2cd0d91a080a0325b60557490edb25458ecb869e
raghav-rama <hackerer528@gmail.com>
2023-10-10 04:27:34 +0530

    feat: add env vars

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config(object):
-    ALCHEMY_API_URL = os.getenv("ALCHEMY_API_URL")
+    ALCHEMY_API_KEY = os.getenv("ALCHEMY_API_KEY")
+    PRIVATE_KEY = os.getenv("PRIVATE_KEY")
